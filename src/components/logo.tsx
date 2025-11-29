cac2e39c0817b303398a48117f2c2a8bcee888e5
Omowumi Badmus <adegbohunomowumi@gmail.com>
2025-11-29 09:28:42 +0000

    remove the icon from from the brand name

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,4 +1,3 @@
-import { Soup } from 'lucide-react';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
 
@@ -9,7 +8,6 @@ type LogoProps = {
 export function Logo({ className }: LogoProps) {
   return (
     <Link href="/" className={cn("flex items-center gap-2", className)}>
-      <Soup className="h-6 w-6 text-primary" />
       <span className="text-xl font-bold font-headline tracking-tighter">
         BeautifulSoup&Foods
       </span>
