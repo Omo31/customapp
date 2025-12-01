26215860c5c44f9dee7daf609154b6d55a906828
Omowumi Badmus <adegbohunomowumi@gmail.com>
2025-12-01 18:15:21 +0000

    beautifulsoupandfoods firebase project have three apps in it with thesam

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,4 @@
+
 import { initializeApp, getApp, getApps } from 'firebase/app';
 
 const firebaseConfig = {
@@ -7,6 +8,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
