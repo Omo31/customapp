a6cc6c495f094a60f84d2a310f9b03871a8b9c0c
Omowumi Badmus <adegbohunomowumi@gmail.com>
2026-01-03 13:56:26 +0000

    firebase deploy --only functions
    === Deploying to 'beautifulsoupandfood

---
+++ src/functions/lib/index.js
@@ -0,0 +1,6 @@
+
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.onNewUser = void 0;
+const on_new_user_1 = require("./on-new-user");
+Object.defineProperty(exports, "onNewUser", { enumerable: true, get: function () { return on_new_user_1.onNewUser; } });
