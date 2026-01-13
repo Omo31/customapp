c053adab323c510039cb07b8a60ef6076472df82
Omowumi Badmus <adegbohunomowumi@gmail.com>
2026-01-13 12:49:20 +0000

    studio-7795331007:~/studio{main}$ firebase deploy --only functions
    ===

---
+++ src/functions/lib/index.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * This file is the main entry point for your Cloud Functions.
+ * You can export multiple functions from this file.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.onNewUser = void 0;
+// Import and re-export the onNewUser function
+var on_new_user_1 = require("./on-new-user");
+Object.defineProperty(exports, "onNewUser", { enumerable: true, get: function () { return on_new_user_1.onNewUser; } });
+//# sourceMappingURL=index.js.map
