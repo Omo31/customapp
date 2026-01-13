515a691385e5a7cfd9df0df1e6a5d8f6537c11bf
Omowumi Badmus <adegbohunomowumi@gmail.com>
2026-01-13 10:16:31 +0000

    i got this message: studio-7795331007:~/studio{main}$ firebase deploy --

--- src/firebase/server-init.ts
+++ src/firebase/server-init.ts
@@ -1,20 +1,16 @@
+
 import { initializeApp, getApps, getApp, cert, App } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY
-  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
-  : undefined;
-
+// When deployed to Firebase, the Admin SDK automatically discovers credentials.
+// Manual parsing of the service account key is not needed and can cause build errors.
 let adminApp: App;
 
 if (getApps().length === 0) {
-  adminApp = initializeApp({
-    credential: serviceAccount ? cert(serviceAccount) : undefined,
-  });
+  adminApp = initializeApp();
 } else {
   adminApp = getApp();
 }
-
 
 const db = getFirestore(adminApp);
 
